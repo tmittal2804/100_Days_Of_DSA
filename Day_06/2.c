e821abbd600c0ac11d0615bffb3de45044392ed4
tmittal2804 <>
2026-02-06 11:15:31 +0530

    ok

---
+++ Day_06/2.c
@@ -0,0 +1,32 @@
+/* Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+1 <= nums.length <= 10^4
+-2^31 <= nums[i] <= 2^31 - 1
+ 
+
+Follow up: Could you minimize the total number of operations done?
+*/
+
+void moveZeroes(int* nums, int numsSize) {
+    int j = 0;   
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != 0) {
+            nums[j++] = nums[i];
+        }
+    }
+    while (j < numsSize) {
+        nums[j++] = 0;
+    }
+}
