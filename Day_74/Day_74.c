94acd8774c087fccaf66fe7f81ee25080beeb739
tmittal2804 <>
2026-04-21 20:58:54 +0530

    ok

---
+++ Day_74/Day_74.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 1000
+#define NAME_LEN 50
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    char names[MAX][NAME_LEN];
+    char unique[MAX][NAME_LEN];
+    int count[MAX] = {0};
+    int uniqueCount = 0;
+
+    for(int i = 0; i < n; i++) {
+        scanf("%s", names[i]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        int found = -1;
+
+        for(int j = 0; j < uniqueCount; j++) {
+            if(strcmp(names[i], unique[j]) == 0) {
+                found = j;
+                break;
+            }
+        }
+
+        if(found != -1) {
+            count[found]++;
+        } else {
+            strcpy(unique[uniqueCount], names[i]);
+            count[uniqueCount] = 1;
+            uniqueCount++;
+        }
+    }
+    int maxVotes = 0;
+    char winner[NAME_LEN];
+
+    for(int i = 0; i < uniqueCount; i++) {
+        if(count[i] > maxVotes) {
+            maxVotes = count[i];
+            strcpy(winner, unique[i]);
+        }
+        else if(count[i] == maxVotes) {
+            if(strcmp(unique[i], winner) < 0) {
+                strcpy(winner, unique[i]);
+            }
+        }
+    }
+
+    printf("%s %d\n", winner, maxVotes);
+
+    return 0;
+}
